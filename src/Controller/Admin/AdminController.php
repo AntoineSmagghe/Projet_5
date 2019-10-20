9f041ef78cb92a6256a01efe8cc595e5fbc0f451
AntoineSmagghe <antoine@smagghe.me>
2019-10-20 21:17:05 +0200

    sigin page and article form sending bug are fixed

--- src/Controller/Admin/AdminController.php
+++ src/Controller/Admin/AdminController.php
@@ -4,13 +4,17 @@ namespace App\Controller\Admin;
 
 use App\Entity\Article;
 use App\Entity\Users;
-use App\Form\ArticleType;
 use App\Repository\UsersRepository;
+use DateTime;
 use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -19,9 +23,8 @@ class AdminController extends AbstractController
     /**
      * @Route("/admin", name="login", methods={"POST", "GET"})
      */
-    public function login(Request $request, ObjectManager $manager, UsersRepository $usersRepository)
+    public function login(Request $request, UsersRepository $usersRepository)
     {
-        
         $users = new Users();
         $form = $this->createFormBuilder($users)
                     ->add('mail', EmailType::class)
@@ -51,23 +54,74 @@ class AdminController extends AbstractController
     }
 
     /**
+     * @Route("/signin", name="signIn", methods={"POST", "GET"})
+     */
+    public function signIn(ObjectManager $manager)
+    {
+        $newUser = new Users();
+        $form = $this->createFormBuilder($newUser)
+            ->add('surname', TextType::class)
+            ->add('name', TextType::class)
+            ->add('mail', EmailType::class)
+            ->add('password', PasswordType::class)
+            ->add('envoyer', SubmitType::class)
+            ->getForm()
+            ;
+
+        if ($form->isSubmitted() && $form->isValid()){
+            $bcryptPass = password_hash($newUser->getPassword(), PASSWORD_BCRYPT);
+            $newUser->setPassword($bcryptPass);
+
+            $manager->persist($newUser);
+            $manager->flush();
+
+            echo "hello world!";
+        }
+
+        return $this->render('admin/signin.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
      * @Route("/admin/edit", name="editPost", methods={"POST", "GET"})
      */
     public function editPost(Request $request, ObjectManager $manager)
     {
-        if(!isset($article)){
-            $article = new Article();
-        }
+        $article = new Article();
 
-        $form = $this->createForm(ArticleType::class, $article);
+        $form = $this->createFormBuilder($article)
+            ->add('date_event', DateType::class, ['widget' => 'single_text'])
+            ->add('format', ChoiceType::class, [
+                'choices' => [
+                    'Event Public' => 'publicEvent',
+                    'Event Privé' => 'privateEvent',
+                    'News' => 'news',
+                    'Release' => 'releases',
+                    'Membres' => 'members'
+            ]])
+            ->add('title', TextType::class)
+            ->add('text', TextareaType::class, ['required' => false])
+            ->add('api_data', TextType::class, ['required' => false])
+            ->add('save', SubmitType::class)
+            ->getForm();
         $form->handleRequest($request);
 
-        if($form->isSubmitted() && $form->isValid()){
-            $manager->persist($form);
-            $manager->flush();
-        }
+        if ($form->isSubmitted() && $form->isValid()){
+            
+            $article->setCreatedAt(new DateTime());
+            $article->setIdAdmin(1);
+            $article->setIdImg(9999999);
 
-        return $this->render('admin/index.html.twig', [
+            $manager->persist($article);
+            $manager->flush();
+
+            return $this->redirectToRoute('article', [
+                'format' => $article->getFormat(), 
+                'id' => $article->getId()
+                ]);
+        }
+        return $this->render('admin/edit_post.html.twig', [
             'form' => $form->createView()
         ]);
     }
