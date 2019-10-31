e617317785a790632e3fa213b7def0a88d34a46d
AntoineSmagghe <antoine@smagghe.me>
2019-10-31 13:23:55 +0100

    try to add a uploader function to avoid image duplication in database

--- src/Service/Uploader.php
+++ src/Service/Uploader.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Service;
 
+use Symfony\Component\Finder\Finder;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
 
@@ -17,23 +18,36 @@ class Uploader
     {
         // Si le filesize($file) est plus le même que le nombre retrouvé dans la BD
         //
-        // Alors on renvoie l'erreur > Image déjà téléchargée l'ami! 
+        // Alors on renvoie l'erreur > Image déjà téléchargée l'ami!
 
-        $originalName = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
-        $safeName = transliterator_transliterate('Any-Latin; Latin-ASCII; [^A-Za-z0-9_] remove; Lower()', $originalName);
-        $uniqueName = $safeName . "-" . filesize($file) . "." . $file->guessExtension();
-        
-        //uniqid()
-        
-        try {
-            $file->move($this->getTargetDirectory(), $uniqueName);
-        } catch (FileException $e) {
-            // --> créer une nouvelle exception 
-            dump($e);
+        $fileSize = filesize($file);
+
+        if (!$this->tchekExistingFile($fileSize)){
+            $originalName = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
+            $safeName = transliterator_transliterate('Any-Latin; Latin-ASCII; [^A-Za-z0-9_] remove; Lower()', $originalName);
+            $uniqueName = $safeName . "-" . $fileSize . "." . $file->guessExtension();
+            
+            //uniqid()
+            
+            try {
+                $file->move($this->getTargetDirectory(), $uniqueName);
+            } catch (FileException $e) {
+                // --> créer une nouvelle exception 
+                dump($e);
+            }
+
+            return $uniqueName;
         }
-        
-        return $uniqueName;
     }
+
+    private function tchekExistingFile($fileSize): bool
+    {
+        $find = new Finder();
+        if ($find->files()->name('*- ' . $fileSize . '.*')){
+            return true;
+        }
+        return false;
+    }   
 
     public function getTargetDirectory() : string
     {
