3f6a3efb08b5bf771bf9c728856d7ba0624205bd
Antoine Smagghe <antoine@smagghe.me>
2019-12-12 14:16:01 +0100

    delete button on soundcloud url field are actived

--- public/js/addField.js
+++ public/js/addField.js
@@ -5,6 +5,7 @@ class addField
         this.ulElement = ulElement;
         this.data_prototype = data_prototype;
         this.index = 0;
+        this.delBtn;
     }
 
     indexValue(){
@@ -15,7 +16,12 @@ class addField
         this.addBtn.addEventListener("click", ()=>{
             let indexedPrototype = this.data_prototype.replace(/__name__/g, this.index);
             let newLi = document.createElement("li");
-            newLi.innerHTML = indexedPrototype;
+            this.delBtn = document.createElement("button");
+            this.delBtn.setAttribute("class", "btn_del_sets");
+            this.delBtn.textContent = "supprimer.";
+            
+            newLi.appendChild(this.delBtn);
+            newLi.insertAdjacentHTML('afterbegin', indexedPrototype);
             /*
             let node = new DOMParser().parseFromString(indexedPrototype, "text/html");
             let nodeInput = node.getElementsByTagName('input')[0];
@@ -24,8 +30,18 @@ class addField
             newLi.appendChild(nodeInput);
             */
             this.ulElement.appendChild(newLi);
+            this.remover();
             this.index++;
         });
+    }
+
+    remover(){
+        let removeBtn = document.getElementsByClassName("btn_del_sets");
+        for (let i = 0; i < removeBtn.length; i++){
+            removeBtn[i].addEventListener("click", (e)=>{
+                e.target.parentNode.parentNode.removeChild(e.target.parentNode);
+            });
+        }
     }
     
     init(){
