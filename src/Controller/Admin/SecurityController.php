771b2a3ed9623a5a8ac0c86ab3eaeb0b534117a2
AntoineSmagghe <antoine@smagghe.me>
2019-12-18 20:38:37 +0100

    add a personnal account page

--- src/Controller/Admin/SecurityController.php
+++ src/Controller/Admin/SecurityController.php
@@ -4,8 +4,11 @@ namespace App\Controller\Admin;
 
 use DateTime;
 use App\Entity\Users;
+use App\Form\UsersType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
@@ -47,7 +50,57 @@ class SecurityController extends AbstractController
     public function signIn(EntityManagerInterface $manager, Request $request)
     {
         $newUser = new Users();
-        $form = $this->createFormBuilder($newUser)
+        $form = $this->createForm(UsersType::class, $newUser);
+        /*
+            ->add('surname', TextType::class)
+            ->add('name', TextType::class)
+            ->add('mail', EmailType::class)
+            ->add('password', PasswordType::class)
+            ->add('confirm_password', PasswordType::class)
+            ->add('roles', CollectionType::class, [
+                'entry_type' => ChoiceType::class,
+                'entry_options' => [
+                    'choices' => [
+                        'Administrateur' => 'ROLE_ADMIN',
+                        'Membre' => 'ROLE_MEMBRE'
+                    ]
+                ]
+            ])
+            ->add('envoyer', SubmitType::class)
+            ->getForm();
+*/
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $bcryptPass = password_hash($newUser->getPassword(), PASSWORD_BCRYPT);
+            $newUser->setPassword($bcryptPass)
+                    ->setLastLog(new DateTime())
+                    ->setCreatedAt(new DateTime());
+
+            $manager->persist($newUser);
+            $manager->flush();
+            $this->addFlash('success', "Le membre a bien été enregistré en base de données.");
+
+            return $this->redirectToRoute("signIn");
+        }
+
+        return $this->render('admin/signin.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/users/account", name="account", methods={"POST", "GET"})
+     */
+    public function account(EntityManagerInterface $manager, Security $security, Request $request)
+    {
+        if ($request->getContent() == null){
+            $user = $security->getUser();
+        } else {
+            $user = new Users();
+        }
+        $form = $this->createForm(UsersType::class, $user);
+        /*
             ->add('surname', TextType::class)
             ->add('name', TextType::class)
             ->add('mail', EmailType::class)
@@ -55,25 +108,23 @@ class SecurityController extends AbstractController
             ->add('confirm_password', PasswordType::class)
             ->add('envoyer', SubmitType::class)
             ->getForm();
-
+*/
         $form->handleRequest($request);
-
+        
         if ($form->isSubmitted() && $form->isValid()) {
-            $bcryptPass = password_hash($newUser->getPassword(), PASSWORD_BCRYPT);
-            $newUser->setPassword($bcryptPass)
-                    ->setLastLog(new DateTime())
-                    ->setCreatedAt(new DateTime())
-                    ->setRoles(['ROLE_MEMBRE']);
+            $bcryptPass = password_hash($user->getPassword(), PASSWORD_BCRYPT);
+            $user->setPassword($bcryptPass);
 
-            $manager->persist($newUser);
+            $manager->persist($user);
             $manager->flush();
-            $this->addFlash('success', "Le membre a bien été enregistré en base de donées.");
+            $this->addFlash('success', "Les changements ont bien été enregistrés en base de données.");
 
-            return $this->redirectToRoute("signIn");
+            return $this->redirectToRoute("account");
         }
 
-        return $this->render('admin/signin.html.twig', [
-            'form' => $form->createView()
+        return $this->render('users/account.html.twig', [
+            'user' => $security->getUser(),
+            'form' => $form->createView(),
         ]);
     }
 }
---
+++ src/Form/UsersType.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Users;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UsersType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('surname', TextType::class)
+            ->add('name', TextType::class)
+            ->add('mail', EmailType::class)
+            ->add('password', PasswordType::class)
+            ->add('confirm_password', PasswordType::class)
+            ->add('roles', CollectionType::class, [
+                'entry_type' => ChoiceType::class,
+                'entry_options' => [
+                    'choices' => [
+                        'Administrateur' => 'ROLE_ADMIN',
+                        'Membre' => 'ROLE_MEMBRE'
+                    ]
+                ]
+            ])
+            ->add('envoyer', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Users::class,
+        ]);
+    }
+}
