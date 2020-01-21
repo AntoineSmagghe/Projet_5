a65b2a09421cdcb668e5b13b91c9c808f199948e
AntoineSmagghe <antoine@smagghe.me>
2020-01-21 17:11:14 +0100

    fix bug with the ajax request of soundcloud's sets. The order of the sets was not good

--- templates/include/soundcloudID.js
+++
@@ -1,4 +0,0 @@
-//Soundcloud ID
-SC.initialize({
-        client_id: 'h3reaiIhlDG8GqF3EAhi6mQWjWREp0LQ'
-    });
--- templates/include/soundcloudResolve.js
+++
@@ -1,4 +0,0 @@
-SC.resolve(" {{sound}} ")
-    .then((track) => {
-        document.getElementsByClassName("widget_SC")[0].insertAdjacentHTML("beforeend", '<iframe width="100%" height="20" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/' + track.id + '&color=%230e0d26&inverse=true&auto_play=false&show_user=true"></iframe>');
-    });
