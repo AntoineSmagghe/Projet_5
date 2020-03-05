a1774dfceb58612dc731bf83cca906c5207a9559
AntoineSmagghe <antoine@smagghe.me>
2020-03-05 15:28:36 +0100

    add tests for home pbulic controller roads

--- tests/Controller/HomePublicControllerTest.php
+++ tests/Controller/HomePublicControllerTest.php
@@ -6,11 +6,20 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class HomePublicControllerTest extends WebTestCase
 {
+    public function testlocalRedirect()
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/');
+
+        $this->assertEquals(302, $client->getResponse()->getStatusCode());
+    }
+
     public function testIndex()
     {
         $client = static::createClient();
 
-        $crawler = $client->request('GET', '/fr/');
+        $client->request('GET', '/fr/');
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
     }
